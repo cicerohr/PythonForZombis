fcf2cce1afa25a68fed5e42e2c9cc39e990e10b2
cicerohr <cicerohr@gmail.com>
2018-09-26 11:09:31 +0200

    Commit inicial

---
+++ _lixo.py
@@ -0,0 +1,6 @@
+# Projeto PythonForZombis. _lixo.py criado por Cicero em 2018-09-26.
+# $$ - $$
+# 
+cores = ['azul', 'amarelo', 'verde']
+for i in range(0, len(cores)):
+    print(f'Índice {i} tem {cores[i]}.')
